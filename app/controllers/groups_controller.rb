4666865ef55a52539e4854d06470ae43cc0205e1
rachel_homeland <rachel.zhuang81@gmail.com>
2017-03-25 22:07:59 +0800

    Join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -46,6 +46,32 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group = GROUP.FIND(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:waring] = "你已经是本讨论版成员了！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:alert] = "加入本讨论版成功！"
+    else
+      flash[:waring] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+    redirect_to group_path(@group)
+  end
+
 
 
   private
